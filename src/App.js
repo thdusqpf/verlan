0afed95571f22f96dc9825201dc4d8ecb09acfa1
sseoni <82037889+sseoni@users.noreply.github.com>
2024-03-04 23:19:54 +0900

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,143 @@
+import logo from './logo.svg';
+import './App.css';
+
+<head>
+    <link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet"/>
+    <link href="./css/main.css" rel="stylesheet"/>
+    <title>Document</title>
+</head>
+
+function App() {
+  return (
+    <div className="v27_65">
+        <div className="topBar">
+            <div className="v27_69"></div>
+            <span className="v27_70">Verlan Dictionary</span>
+            <div className="v27_71">
+                <div className="v27_72">
+                    <span className="v27_73">Search in site</span>
+                    <div className="v27_74">
+                        <div className="v27_75"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div className="v27_76">
+            <div className="v27_77">
+                <span className="v27_78">Search</span>
+                <div className="v27_79">
+                    <span className="v27_80">Search Word</span>
+                    <div className="v27_81">
+                        <span className="v27_82">Enter a word</span>
+                    </div>
+                </div>
+                <div className="v27_83">
+                    <div className="v27_84">
+                        <span className="v27_85">Search</span>
+                    </div>
+                </div>
+            </div>
+            <div className="v27_86"></div>
+        </div>
+        <div className="v27_87">
+            <div className="v27_88">
+                <span className="v27_89">Word Cloud</span>
+                <div className="v27_90">
+                    <div className="v27_91">
+                        <span className="v27_92">Save</span>
+                    </div>
+                </div>
+            </div>
+            <div className="v27_93">
+                <div className="v27_94">
+                    <div className="v27_95">
+                        <div className="v27_96">
+                            <div className="v27_97"></div>
+                        </div>
+                        <div className="v27_98">
+                            <span className="v27_99">Word</span>
+                            <span className="v27_100">Definition of the word</span>
+                            <div className="v27_101">
+                                <div className="v27_102">
+                                    <span className="v27_103">Noun</span>
+                                </div>
+                            </div>
+                            <div className="v27_104">
+                                <div className="v27_105">
+                                    <div className="v27_106"></div>
+                                    <div className="v27_107">
+                                        <span className="v27_108">Dictionary</span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="v27_109">
+                    <div clclassNameass="v27_110">
+                        <div className="v27_111">
+                            <div className="v27_112"></div>
+                        </div>
+                        <div className="v27_113">
+                            <span className="v27_114">Example</span>
+                            <span className="v27_115">Sample sentence using the word.</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="v27_116"></div>
+        </div>
+        <div className="v27_117">
+            <div className="v27_118">
+                <span className="v27_119">Word Cloud</span>
+            </div>
+            <div className="v27_120">
+                <div className="v27_121">
+                    <div className="v27_122">
+                        <div className="v27_123">
+                            <div className="v27_124"></div>
+                            <div className="v27_125">
+                                <span className="v27_126">User123</span>
+                            </div>
+                        </div>
+                    </div>
+                    <span className="v27_127">Great explanation of the word.</span>
+                </div>
+                <div className="v27_128">
+                    <div className="v27_129">
+                        <div className="v27_130">
+                            <div className="v27_131"></div>
+                            <div className="v27_132">
+                                <span className="v27_133">WordLover</span>
+                            </div>
+                        </div>
+                    </div>
+                    <span className="v27_134">Helpful definitions.</span>
+                </div>
+            </div>
+            <div className="v27_135"></div>
+        </div>
+        <div className="v27_136">
+            <div className="v27_137">
+                <div className="v27_138">
+                    <span className="v27_139">Explore more words with our dictionary!</span>
+                    <div className="v27_140">
+                        <div className="v27_141"></div>
+                        <div className="v27_142"></div>
+                        <div className="v27_143"></div>
+                        <div className="v27_144"></div>
+                    </div>
+                </div>
+            </div>
+            <div className="v27_145"></div>
+        </div>
+        <div className="v27_146">
+            <div className="v27_147">
+                <span className="v27_148">2024 Verlan Dictionary. All Rights Reserved.</span>
+            </div>
+        </div>
+    </div>
+  );
+}
+
+export default App;
